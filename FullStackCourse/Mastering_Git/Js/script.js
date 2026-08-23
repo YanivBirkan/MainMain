2d83c39e8e8325d358cb4fd47970fe3d766dcb16
YanivBirkan <yaniv.birkan@gmail.com>
2026-08-23 23:39:11 +0300

    when ball clicked add width and height

--- FullStackCourse/Mastering_Git/Js/script.js
+++ FullStackCourse/Mastering_Git/Js/script.js
@@ -1,4 +1,10 @@
-function onBallCLick(){
+function onBallCLick(elBall){
     console.log("ball clicked");
+    const currentWidth = parseInt(elBall.style.width) || 100;
+    
+    const newWidth = currentWidth + 50;
+    elBall.style.width = newWidth + "px";
+    elBall.style.height = newWidth + "px";
+    elBall.innerText = elBall.style.width;
     
 }
